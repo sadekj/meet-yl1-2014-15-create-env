dfb6f5ccc45c880467db7c13c84f95d1d99c93b6
Sadek Jabr <sadek.jabr@gmail.com>
2014-11-15 17:35:06 +0200

    Adding the script files

---
+++ create-env.sh
@@ -0,0 +1,5 @@
+echo "This might take a minute or two, DO NOT close the Terminal, until you ask your instructor or TA if it worked"
+
+mkvirtualenv --python=/usr/bin/python3.3 -r /etc/MEET/standard-python-env-requirements.txt yl1
+
+
---
+++ y1-script.sh
@@ -0,0 +1,8 @@
+echo 'This is a file to be ran once and it will be deleted once it finishes, any problems while running it contact me at sadek07@meet.mit.edu'
+
+echo 'source /usr/loca/bin/virtualenvwrapper.sh' >> ~/.bashrc
+
+chmod +x create-env.sh
+
+echo 'Please close this and any other opened terminal, then open it again and type ./create-env.sh'
+
