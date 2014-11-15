12881cd9578230bf0588736017ed545315958f6b
Sadek Jabr <sadek.jabr@gmail.com>
2014-11-15 18:51:55 +0200

    temp fixing

--- create-env.sh
+++ create-env.sh
@@ -1,7 +1,7 @@
 echo "This might take a minute or two, DO NOT close the Terminal, until you ask your instructor or TA if it worked"
 
-sed '$d' ~/.bashrc > ~/.bashrc
-sed '$d' ~/.bashrc > ~/.bashrc
+sed '$d' ~/.bashrc > temp
+sed '$d' temp > ~/.bashrc
 
 mkvirtualenv --python=/usr/bin/python3.3 -r /etc/MEET/standard-python-env-requirements.txt yl1
 
