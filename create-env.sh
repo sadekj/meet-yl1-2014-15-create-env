05ca6ad5091254db89202e0be80382e58fce17e1
Sadek Jabr <sadek.jabr@gmail.com>
2014-11-15 17:46:19 +0200

    added some messages to .bashrc

--- create-env.sh
+++ create-env.sh
@@ -1,5 +1,8 @@
 echo "This might take a minute or two, DO NOT close the Terminal, until you ask your instructor or TA if it worked"
 
+sed '$d' ~/.bashrc
+sed '$d' ~/.bashrc
+
 mkvirtualenv --python=/usr/bin/python3.3 -r /etc/MEET/standard-python-env-requirements.txt yl1
 
-
+echo 'echo Before starting working type workon yl1' >> ~/.bashrc
