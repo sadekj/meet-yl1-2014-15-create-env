e54b29f6592ea4cb966808c289e3d5d1ee25bf10
Sadek Jabr <sadek.jabr@gmail.com>
2014-11-15 17:52:25 +0200

    fixing printing in ./bashrc

--- y1-script.sh
+++ y1-script.sh
@@ -1,8 +1,8 @@
 echo 'This is a file to be ran once and it will be deleted once it finishes, any problems while running it contact me at sadek07@meet.mit.edu'
 
 echo 'source /usr/loca/bin/virtualenvwrapper.sh' >> ~/.bashrc
-echo 'echo Now type this: cd meet-yl1-2014-15-create-env'
-echo 'echo Then type this: ./create-env.sh'
+echo "echo 'Now type this: cd meet-yl1-2014-15-create-env'" >> ~/.bashrc
+echo "echo Then type this: ./create-env.sh'" >> ~/.bashrc
 
 chmod +x create-env.sh
 
